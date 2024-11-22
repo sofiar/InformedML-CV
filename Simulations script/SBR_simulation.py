0ddc0c3a9a0950221430f656a68ee23ddc3268b6
sofiar <sofi.ruizsuarez@gmail.com>
2024-11-22 17:08:26 -0500

    parallelize code

--- Simulations script/SBR_simulation.py
+++ Simulations script/SBR_simulation.py
@@ -3,6 +3,7 @@ import sys
 import os
 from typing import Dict, List, Tuple
 import joblib
+from joblib import Parallel, delayed
 import numpy as np
 import torch 
 import torch.nn as nn
@@ -14,6 +15,9 @@ sys.path.append(modular_path)
 from modular import engine
 import modular.samples_setup as cs
 from modular import model_builder
+
+# set number of cores
+torch.set_num_threads(100)
 
 # Define functions 
 def semantic_regularization_loss(logits):
@@ -158,13 +162,62 @@ def train_test_loop_reg(model: torch.nn.Module,
     return results
 
 
-# Define variables and loop 
+# Parallelize loop 
+def process_combination(v, var, n, nsample, k, p_errors, alphas):
+    noise_p = p_errors[v]
+    # 1. Simulate images and labels
+    output = cs.generate_sample(n=nsample,  
+                                noise_prop = noise_p, var=var,
+                                Nclass=3)
+    images, labels= (output['images'], output['labels'])
+    
+    # 2. Generate dataset
+    train_dataset, test_dataset = cs.generate_dataset(images, labels)
+    train_dataloader = DataLoader(train_dataset, 
+                        batch_size= 32, 
+                        shuffle=True)
+    test_dataloader = DataLoader(test_dataset,
+                        batch_size= 32,
+                        shuffle=True )
+                
+    # 3. Load model
+    model = model_builder.TVGG(input_shape = 1,
+                        hidden_units = 10, 
+                        output_shape = 3)
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
+    loss_fn = nn.CrossEntropyLoss()
+    
+    outputs = []
+    for a, alpha in enumerate(alphas):
+        # 4. Run model
+        output = train_test_loop_reg(model = model,
+                                    train_dataloader = train_dataloader,
+                                    test_dataloader = test_dataloader,
+                                    optimizer = optimizer,
+                                    loss_fn = loss_fn, 
+                                    epochs = EPOCHS, 
+                                    print_b=False,
+                                    alpha = alpha)
 
-alphas = [0]#[0,0.05,0.1,0.2,0.3,0.4,0.5]
-NREPS = 10 #50
-n_samples = [[50]*3]#[[800,3000,3200],[3000,1000,5000],[2500]*3,[5000]*3]
-var_errors = [0,1]#[0,0.15,0.15] 
-prop_errors = [0,0.5]#[0,0.5,1]
+        acc_test = output['test_acc'][EPOCHS-1]
+        ce_test = output['test_ce'][EPOCHS-1]            
+        outputs.append((a, acc_test,ce_test))
+
+    # Print message 
+    if k % 50 == 0:  
+        print(f"Combination ({v}, {var}, {n}, {nsample}) done!")
+   
+    return (v, n, k, outputs)
+
+            
+    
+# Define variables
+
+alphas = [0,0.05,0.1,0.2,0.3]
+NREPS = 50
+n_samples = [[800,3000,3200],[3000,1000,5000],[2500]*3,[5000]*3]
+var_errors = [0,0.15,0.15] 
+prop_errors = [0,0.5,1]
 
 # Keep ce and test accuracy
 accuracy_test = np.zeros((len(var_errors),len(alphas), len(n_samples) ,NREPS))
@@ -174,47 +227,27 @@ BATCH_SIZE = 50
 EPOCHS = 6
 
 
-for v, var in enumerate(var_errors):
-    noise_p = prop_errors[v]
-    for n, nsample in enumerate(n_samples):
-        for a, alpha in enumerate(alphas):
-            for k in range(NREPS):
-                # 1. Simulate images and labels
-                output = cs.generate_sample(n=nsample, seed=11, 
-                                            noise_prop = noise_p, var=var,
-                                            Nclass=3)
-                images, labels= (output['images'], output['labels'])
-                
-                
-                # 2. Generate dataset
-                train_dataset, test_dataset = cs.generate_dataset(images, labels)
-                train_dataloader = DataLoader(train_dataset, 
-                                    batch_size= 32, 
-                                    shuffle=True)
-                test_dataloader = DataLoader(test_dataset,
-                                    batch_size= 32,
-                                    shuffle=True )
-                # 3. Load model
-                model = model_builder.TVGG(input_shape = 1,
-                                    hidden_units = 10, 
-                                    output_shape = 3)
-                optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
-                loss_fn = nn.CrossEntropyLoss()
-                
-                # 4. Run model
-                output = train_test_loop_reg(model = model,
-                                             train_dataloader = train_dataloader,
-                                             test_dataloader = test_dataloader,
-                                             optimizer = optimizer,
-                                             loss_fn = loss_fn,                                            epochs = EPOCHS, 
-                                             print_b=False,
-                                             alpha = alpha)
-                
-                accuracy_test[v,a,n,k]  = output['test_acc'][EPOCHS-1]
-                ce_test[v,a,n,k]  = output['test_ce'][EPOCHS-1]           
+# Parallelize the outermost loop
+outputs = Parallel(n_jobs=100)(delayed(process_combination)(v, var, n, nsample,  
+                               k, p_errors = prop_errors,
+                               alphas = alphas)
+                    for v, var in enumerate(var_errors)
+                    for n, nsample in enumerate(n_samples)
+                    for k in range(NREPS))
 
-# Save variables using joblib 
+for v, n, k, results in outputs:
+    for a, acc, ce in results:
+        accuracy_test[v,a,n,k]  = acc
+        ce_test[v,a,n,k]  = ce
 
+
+# for v, n, a, k, ac_t, ce_t in outputs:
+#     accuracy_test[v,a,n,k]  = ac_t
+#     ce_test[v,a,n,k]  = ce_t
+     
+
+
+# Save environment
 env_vars = {'n_samples' : n_samples,
             'var_errors': var_errors,
             'alphas': alphas,
@@ -225,5 +258,10 @@ env_vars = {'n_samples' : n_samples,
             'BATCH_SIZE': BATCH_SIZE
             }
 
-joblib.dump(env_vars, '/home/sofia/Candu_postdoc/InformedML-CV/environments/SBR_Nov21.pkl')
+# Save variables using joblib 
+joblib.dump(env_vars, '/u/ruizsuar/InformedML-CV/Environments/SBR_Nov21.pkl')
 
+
+# Save the dictionary using pickle
+# with open('./environments/SBR_Nov21.pkl', 'wb') as f:
+#     pickle.dump(env_vars, f)

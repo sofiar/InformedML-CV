20c6faf546f5b23ded13bce15c23371788ca836d
sofiar <sofi.ruizsuarez@gmail.com>
2024-12-20 14:29:47 -0500

    rename folder for importation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="InformedMlCv",                
+    version="0.1",                   
+    packages=find_packages(),       
+    description="A library to explore Informed machine learning techniques for computer vision tasks.",  
+    author="Sofia Ruiz-Suarez",
+    url="https://github.com/sofiar/InformedML-CV",
+    include_package_data=True
+    )

4149054f2a03eed8f3b60d702fdac4f96e4fbcce
sofia <sofi.ruizsuarez@gmail.com>
2025-07-22 13:02:59 -0400

    improve identation and style samples_setup

--- modular/samples_setup.py
+++ modular/samples_setup.py
@@ -2,14 +2,22 @@
 Contains functionality for genenrating and setting up samples for 
 image classification data.
 """
-import random
 import numpy as np
 from sklearn.model_selection import train_test_split
 import torch
 import modular.extra_functions as ef
 
-def generate_sample(noise_prop, seed = 999,size = 28, var = 0.15,
-                    noise_indx = False, order = None,n=None, Nclass = 2):
+def generate_sample(
+    noise_prop, 
+    seed = 999,
+    size = 28, 
+    var = 0.15,
+    noise_indx = False, 
+    order = None,
+    n=None, 
+    Nclass = 2
+    ):
+    
     """Generates samples.
 
      Args:
@@ -31,8 +39,10 @@ def generate_sample(noise_prop, seed = 999,size = 28, var = 0.15,
   """
     
     if(n is not None and order is not None):
-        print("Error: n and order cannot be input at the same time" +
-              "please select only one method")
+        print(
+            "Error: n and order cannot be input at the same time" +
+            "please select only one method"
+        )
         
     if(Nclass not in [2,3]):
         print("Error: Nclass must be 2 or 3")      
@@ -48,10 +58,12 @@ def generate_sample(noise_prop, seed = 999,size = 28, var = 0.15,
                 ntot = sum(n)
                 n_cr = n[0]
                 n_sq = n[1]
+                
                 for _ in range(n_cr):
-                    radius = np.random.randint(2, size // 2)  #
+                    radius = np.random.randint(2, size // 2)  
                     image = ef.draw_circle(size, radius)
                     dataset_circles.append(image)
+                    
                 for _ in range(n_sq):
                     len_size = np.random.randint(2, size // 2)  
                     image = ef.draw_square(size, len_size)
@@ -77,28 +89,34 @@ def generate_sample(noise_prop, seed = 999,size = 28, var = 0.15,
                 n_sq = np.sum(order == 1)
                 n_cr = np.sum(order == 0)
                 ntot = n_cr + n_sq
+                
                 for _ in range(n_sq):
                     len_size = np.random.randint(2, size // 2)  # Random len between 2 and half the image size
                     image = ef.draw_square(size, len_size)
                     dataset_squares.append(image)
+                
                 for _ in range(n_cr):    
                     radius = np.random.randint(2, size // 2)  # Random radius between 2 and half the image size
                     image = ef.draw_circle(size, radius)
                     dataset_circles.append(image)
+            
             if(Nclass==3):
                 dataset_triangles = []    
                 n_sq = np.sum(order == 1)
                 n_cr = np.sum(order == 0)
                 n_tr = np.sum(order == 2)
                 ntot = n_cr + n_sq + n_tr
+            
                 for _ in range(n_sq):
                     len_size = np.random.randint(2, size // 2)  # Random len between 2 and half the image size
                     image = ef.draw_square(size, len_size)
                     dataset_squares.append(image)
+            
                 for _ in range(n_cr):    
                     radius = np.random.randint(2, size // 2)  # Random radius between 2 and half the image size
                     image = ef.draw_circle(size, radius)
                     dataset_circles.append(image)
+            
                 for _ in range(n_tr):
                     t_size = np.random.randint(2, size // 2)  # Random len between 2 and half the image size
                     image = ef.draw_triangle(size, t_size)
@@ -112,9 +130,16 @@ def generate_sample(noise_prop, seed = 999,size = 28, var = 0.15,
         noisy_indices_s = np.random.choice(n_sq, n_noisy_sq, replace=False)       
               
         for i in noisy_indices_c:
-            dataset_circles[i] = ef.add_gaussian_noise(dataset_circles[i],var=var)
+            dataset_circles[i] = ef.add_gaussian_noise(
+                dataset_circles[i],
+                var=var
+            )
+            
         for i in noisy_indices_s:
-            dataset_squares[i] = ef.add_gaussian_noise(dataset_squares[i],var=var)
+            dataset_squares[i] = ef.add_gaussian_noise(
+                dataset_squares[i],
+                var=var
+            )
             
         # Create labels
         circle_labels = np.full(n_cr, 0)
@@ -128,8 +153,12 @@ def generate_sample(noise_prop, seed = 999,size = 28, var = 0.15,
             np.random.seed(seed)
             n_noisy_tr = int(n_tr * noise_prop)
             noisy_indices_t = np.random.choice(n_tr, n_noisy_tr, replace=False)       
+            
             for i in noisy_indices_t:
-                dataset_triangles[i] = ef.add_gaussian_noise(dataset_triangles[i],var=var)
+                dataset_triangles[i] = ef.add_gaussian_noise(
+                    dataset_triangles[i],
+                    var=var
+                )
             # Create labels
             triangle_labels = np.full(n_tr, 2)
             
@@ -143,6 +172,7 @@ def generate_sample(noise_prop, seed = 999,size = 28, var = 0.15,
             all_images[indx_cr] = dataset_circles
             all_labels[indx_sq] = square_labels
             all_labels[indx_cr] = circle_labels
+            
             if (Nclass==3):
                 indx_tr = np.where(order==2)
                 all_images[indx_tr] = dataset_triangles
@@ -160,17 +190,24 @@ def generate_sample(noise_prop, seed = 999,size = 28, var = 0.15,
         
         # recover noisy indices
         if noise_indx:
-            noisy_indx  = np.concatenate((noisy_indices_c,ntot + noisy_indices_s))    
-            output = {"images": all_images,
-                    "labels": all_labels,
-                    "indx_noisy_data": noisy_indx}
+            noisy_indx  = np.concatenate(
+                (noisy_indices_c,
+                 ntot + noisy_indices_s)
+                )
+                
+            output = {
+                "images": all_images,
+                "labels": all_labels,
+                "indx_noisy_data": noisy_indx
+                }
         else:
-            output = {"images": all_images,
-                    "labels": all_labels}
+            output = {
+                "images": all_images,
+                "labels": all_labels
+                }
+            
         return output
-        #return all_images, all_labels
-
-
+        
 
 def generate_dataset(all_images, all_labels, test_size=0.2,seed = 999):
 
@@ -192,8 +229,12 @@ def generate_dataset(all_images, all_labels, test_size=0.2,seed = 999):
     X = torch.from_numpy(all_images).type(torch.float)
     Y = torch.from_numpy(all_labels).type(torch.long)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size,
-                                                    random_state=seed) 
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        Y,
+        test_size=test_size,
+        random_state=seed
+    ) 
 
     # Add channel at dimension 1 (greyscale)
     X_train = X_train.unsqueeze(1)  
